0fa9b28efe554f4b561f1dfcbd32f432be0e4170
Mohamed Baig <mohamed.b.baig@gmail.com>
2017-10-08 20:38:25 -0400

    added deploy script and https for fixer API

--- src/utils/fixer.js
+++ src/utils/fixer.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 async function getRates(fromCurrency, toCurrency) {
     try {
-        const url = `http://api.fixer.io/latest?base=${fromCurrency}&symbols=${toCurrency}`;
+        const url = `https://api.fixer.io/latest?base=${fromCurrency}&symbols=${toCurrency}`;
         const response = await axios.get(url);
         return response;
     } catch (error) {
