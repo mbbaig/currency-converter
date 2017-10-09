4b264973e32eae6aa47f6364266cba9af4b70f2c
Mohamed Baig <mohamed.b.baig@gmail.com>
2017-10-08 20:15:05 -0400

    update README and manifest information

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -3,7 +3,7 @@ import CurrencyConverter from '../currencyConverter/CurrencyConverter';
 
 function App() {
     return (
-        <div className="App">
+        <div>
             <CurrencyConverter />
             <CurrencyConverter />
             <CurrencyConverter />
